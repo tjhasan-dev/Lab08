74336a98deacc6303227fadd6d77bbbf6cbad932
Tasmiah Hasan <tjhasan@ualberta.ca>
2026-03-01 17:40:29 -0700

    Implement hasCity method

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,7 @@
 package com.example.lab08;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomListTest {
 
