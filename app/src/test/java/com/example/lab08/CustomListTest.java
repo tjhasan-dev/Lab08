75f90df9a88d7ab2bfd62a6e30bd9593eff2581b
Tasmiah Hasan <tjhasan@ualberta.ca>
2026-03-01 17:53:14 -0700

    Add failing test for countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -25,4 +25,16 @@ public class CustomListTest {
         assertFalse(list.hasCity(edmonton));
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+
+        assertEquals(0, list.countCities());
+
+        list.addCity(new City("Edmonton", "AB"));
+        list.addCity(new City("Calgary", "AB"));
+
+        assertEquals(2, list.countCities());
+    }
+
 }
